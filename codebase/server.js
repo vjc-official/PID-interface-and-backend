52b7895e24fa54d3692281556dec0ee9128c4b7b
Kesseki <98925679+vjc-official@users.noreply.github.com>
2026-02-08 21:31:44 +0800

    As of 2/8/26 2130

--- codebase/server.js
+++ codebase/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mqtt = require("mqtt");
 const path = require("path");
+const { pool } = require("pg");
 
 const app = express();
 //server port
@@ -18,27 +19,28 @@ client.on("connect", () => {
 
 // Middleware to parse JSON from frontend
 app.use(express.json());
-app.use(express.static('public')); // Serves your HTML file
+app.use(express.static("public")); // Serves your HTML file
 
-app.post('/api/command', (req, res) => {
-    const { kp, ki, kd, set } = req.body;
-    
-    const payload = JSON.stringify({ kp, ki, kd, set });
-    
-    client.publish("commandData", payload, (err) => {
-        if (err) {
-            return res.status(500).json({ error: "Failed to publish" });
-        }
-        res.json({ status: "Command sent!" });
-    });
+app.post("/api/command", (req, res) => {
+  const { kp, ki, kd, set } = req.body;
+
+  const payload = JSON.stringify({ kp, ki, kd, set });
+
+  client.publish("commandData", payload, (err) => {
+    if (err) {
+      return res.status(500).json({ error: "Failed to publish" });
+    }
+    res.json({ status: "Command sent!" });
+  });
 });
 
 // For receiving MQTT data
 client.on("message", (topic, message) => {
-
   try {
     const data = JSON.parse(message);
-      console.log(`Rise Time: ${data.riseTime}, Settling Time: ${data.settlingTime}, Steady State Error: ${data.steadyStateError}, Overshoot: ${data.overshoot}, Setpoint: ${data.setpoint}, Actual Value: ${data.actualValue}`);
+    console.log(
+      `Rise Time: ${data.riseTime}, Settling Time: ${data.settlingTime}, Steady State Error: ${data.steadyStateError}, Overshoot: ${data.overshoot}, Setpoint: ${data.setpoint}, Actual Value: ${data.actualValue}`,
+    );
   } catch (error) {
     console.error("Received message was not valid JSON");
   }
